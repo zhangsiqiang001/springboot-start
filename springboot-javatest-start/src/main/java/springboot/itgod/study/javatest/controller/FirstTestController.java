71c0f0ceecc503c90f90a70cdffe09232a0948db
root <1218542440@qq.com>
2021-07-11 22:18:11 +0800

    开启工作队列

--- springboot-javatest-start/src/main/java/springboot/itgod/study/javatest/controller/FirstTestController.java
+++ springboot-javatest-start/src/main/java/springboot/itgod/study/javatest/controller/FirstTestController.java
@@ -1,4 +1,4 @@
-package springboot.itgod.study.controller;
+package springboot.itgod.study.javatest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
