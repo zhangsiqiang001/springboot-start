9ad7747aad34cd67e7094737001ce8b549801c0b
张思强 <1218542440@qq.com>
2021-07-18 22:42:43 +0800

    spring对象初始化之后，执行初始化数据方法（spring学习）

--- springboot-springframework-start/src/main/java/springboot/itgod/study/springframework/app/Appconfig.java
+++ springboot-springframework-start/src/main/java/springboot/itgod/study/springframework/app/Appconfig.java
@@ -1,4 +1,13 @@
 package springboot.itgod.study.springframework.app;
 
 public class Appconfig {
+
+    /**
+     *
+     * spring 生命周期回调实现三种方式：
+     * 1.加@PostConstruct
+     * 2.实现 InitializingBean接口，实现afterPropertiesSet
+     * 3.init-method=""
+     */
+
 }
